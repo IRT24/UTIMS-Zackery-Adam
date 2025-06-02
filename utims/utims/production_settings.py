6f58bdbe391634f0ab4ef5b3b419db1580ebba27
IRT24 <kl2007007391@student.kuptm.edu.my>
2025-06-02 21:28:26 +0800

    Update production_settings.py

--- utims/utims/production_settings.py
+++ utims/utims/production_settings.py
@@ -101,7 +101,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-]
+] if os.path.exists(os.path.join(BASE_DIR, 'static')) else []
 
 # Media files settings
 MEDIA_URL = '/media/'
